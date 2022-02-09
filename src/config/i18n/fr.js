841ac64092d667289009dfaa84cce761b2a8be31
pyphilia <pyphilia@gmail.com>
2022-02-09 17:10:50 +0100

    refactor: add french translations

--- src/config/i18n/fr.js
+++ src/config/i18n/fr.js
@@ -1,30 +1,34 @@
 const fr = {
   translations: {
-    Home: '',
-    Simulations: '',
-    Team: '',
+    Home: 'Accueil',
+    Simulations: 'Simulations',
+    Team: 'Equipe',
     'Active learning simulations for investigating the science of global warming':
-      '',
+      "Simulations d'apprentissage actif pour comprendre la science derrière le réchauffement climatique",
     'Based on climate education research • Adapted for middle and high schools • Tested in classrooms':
-      'A • B • C',
-    '• Easily embedded in Graasp activities and other web platforms': '• A',
-    'Oscillating charge': '',
-    'Electromagnetic waves emitted by an oscillating charge': '',
-    'Thermal radiation': '',
-    'Thermal radiation emitted by a planetary surface': '',
-    'Radiation absorption': '',
-    'Interaction between radiation and gas molecules': '',
+      "Basées sur la recherche dans l'apprentissage sur le climat • Adaptées pour l'école primaire et secondaire • Testées dans des classes",
+    '• Easily embedded in Graasp activities and other web platforms':
+      "• Facilement intégrées dans des activités sur Graasp et d'autres plateformes web",
+    'Oscillating charge': 'Charge oscillante',
+    'Electromagnetic waves emitted by an oscillating charge':
+      'Ondes électromagnétiques émises par une charge oscillante',
+    'Thermal radiation': 'Rayonnement Thermique',
+    'Thermal radiation emitted by a planetary surface':
+      'Rayonnement thermique émis par la surface planétaire',
+    'Radiation absorption': 'Absorption de rayonnement',
+    'Interaction between radiation and gas molecules':
+      'Intéraction entre les rayonnements et les molécules de gaz',
     '* Recommended pedagogical prerequisites: Concepts of electric charges, atoms, molecules, energy, and power':
-      '* ',
+      '* Prérequis pédagogiques recommendés: Concepts sur les charges électriques, atomes, molécules, énergie et puissance',
     'The simulations were designed and validated by <2>Philippe Kobel</2> and <6>Thierry Gerez</6> (Physics and Chemistry teachers at Gymnase du Bugnon), with the scientific collaboration of <10>Stéphane Goyette</10> (University of Geneva) and didactic support of Yves Debernardi and <14>Nicolas Perrin</14> (Haute École Pédagogique Vaud). The interaction design and implementation were carried out by <18>Kim Lan Phan Hoang</18> and <22>Hagop Taminian</22> from the Graasp team (École Polytechnique Fédérale de Lausanne and Graasp.org).':
-      '<2>Philippe Kobel</2> <6>Thierry Gerez</6> <10>Stéphane Goyette</10> <14>Nicolas Perrin</14> <18>Kim Lan Phan Hoang</18> <22>Hagop Taminian</22>',
-    'Contact us': '',
-    'Your Name': '',
-    'Your Email': '',
-    'Your Message': '',
-    Submit: '',
-    'Supported by': '',
-    Copyright: '',
+      'Les simulations ont été conçues et validées par <2>Philippe Kobel</2> <6>Thierry Gerez</6> <10>Stéphane Goyette</10> <14>Nicolas Perrin</14> <18>Kim Lan Phan Hoang</18> et <22>Hagop Taminian</22> du groupe de Graasp (École Polytechnique Fédérale de Lausanne et Graasp.org)',
+    'Contact us': 'Nous Contacter',
+    'Your Name': 'Nom',
+    'Your Email': 'Email',
+    'Your Message': 'Message',
+    Submit: 'Envoyer',
+    'Supported by': 'Supported by',
+    Copyright: 'Copyright',
   },
 };
 
