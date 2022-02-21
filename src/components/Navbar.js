f3da51f667cb3d9c40a66951d13514b06b2b6b88
Hagop Taminian <hagop.taminian@gmail.com>
2022-02-21 13:27:08 +0200

    fix: fix header issue on smaller screens
    closes #23

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,15 +8,10 @@ function Navbar() {
   const [language, setLanguage] = useState('en');
   const { t } = useTranslation();
 
-  const textStyle = {
-    color: '#cdc3f3',
-    fontWeight: '600',
-    letterSpacing: '0.1em',
-  };
   return (
     <div className="pos-f-t">
       <nav className="navbar navbar-dark navbar-expand-md">
-        <h4 style={textStyle}>GRAASP CLIMATE</h4>
+        <h4 className="logo-text">GRAASP CLIMATE</h4>
         <button
           className="navbar-toggler"
           type="button"
