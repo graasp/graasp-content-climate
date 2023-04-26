1e9c7c31fdd4c38a9affab5f991b142a88e14f5d
pyphilia <pyphilia@gmail.com>
2023-04-26 10:26:08 +0300

    refactor: add mock item id for standalone preview

--- src/subpages/Services.js
+++ src/subpages/Services.js
@@ -5,7 +5,8 @@ import ServiceItem from '../components/ServiceItem';
 function Services() {
   const { t, i18n } = useTranslation();
 
-  const lang = `?lang=${i18n.language}`;
+  // item id is necessary for standalone preview
+  const qs = `?lang=${i18n.language}&itemId=itemId`;
 
   return (
     <div
@@ -32,7 +33,7 @@ function Services() {
               description={t(
                 'Electromagnetic waves emitted by an oscillating charge',
               )}
-              web={`https://apps.graasp.org/4b63857b-1bd9-44cf-a72d-4fb7ff531b13/latest/index.html${lang}`}
+              web={`https://apps.graasp.org/4b63857b-1bd9-44cf-a72d-4fb7ff531b13/latest/index.html${qs}`}
               github="https://github.com/graasp/graasp-app-radiating-charge"
             />
             <ServiceItem
@@ -40,13 +41,13 @@ function Services() {
               description={t(
                 'Thermal radiation emitted by a planetary surface',
               )}
-              web={`https://apps.graasp.org/ff4d50d9-1370-4707-a460-02a041474579/index.html${lang}`}
+              web={`https://apps.graasp.org/ff4d50d9-1370-4707-a460-02a041474579/index.html${qs}`}
               github="https://github.com/graasp/graasp-app-thermal-radiation"
             />
             <ServiceItem
               title={t('Radiation absorption')}
               description={t('Interaction between radiation and gas molecules')}
-              web={`https://apps.graasp.org/35288bda-d787-42f9-b5d6-110d689d0eda/index.html${lang}`}
+              web={`https://apps.graasp.org/35288bda-d787-42f9-b5d6-110d689d0eda/index.html${qs}`}
               github="https://github.com/graasp/graasp-app-radiation-absorption"
             />
             <ServiceItem
@@ -54,7 +55,7 @@ function Services() {
               description={t(
                 'Global model including radiation, greenhouse gases, clouds and ice',
               )}
-              web={`https://apps.graasp.org/f61d6916-be33-4c63-b697-2c7b09a7b8e5/latest/index.html${lang}`}
+              web={`https://apps.graasp.org/f61d6916-be33-4c63-b697-2c7b09a7b8e5/latest/index.html${qs}`}
               github="https://github.com/graasp/graasp-app-greenhouse-effect"
             />
           </div>
